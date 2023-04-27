7ab2390097dd0a5422785f0353aee19e5641cf1c
Edge <tusharavhad849@gmail.com>
2023-04-27 19:51:48 +0530

    date-location added

--- static/js/toggle_table.js
+++ static/js/toggle_table.js
@@ -1,15 +1,48 @@
 function toggleTableRows(checkbox) {
     const rows = document.querySelectorAll('table tbody tr');
-    
+    const locationValue_ = document.getElementById("location-filter").value.toLowerCase();
+    const dateValue_ = document.getElementById("date-filter").value;
     for (let i = 0; i < rows.length; i++) {
       const flagCell = rows[i].querySelector('td:nth-child(9)');
       if (checkbox.checked && flagCell && flagCell.textContent.trim() !== '1') {
         rows[i].style.display = 'none';
-      } else  {
+      } else if(locationValue_!='') {
+        if(rows[i].querySelector('td:nth-child(6)').textContent.toLowerCase() == locationValue_)
+        {
+          rows[i].style.display='';
+        }
+        else
+        {
+          rows[i].style.display = 'none';
+
+        }
+      }
+      else if(dateValue_!='')
+      {
+        if(rows[i].querySelector('td:nth-child(7)').textContent == dateValue_)
+        {
+
+          rows[i].style.display='';
+        }
+        else
+        {
+          rows[i].style.display = 'none';
+        }
+
+      }
+      else
+      {
         rows[i].style.display = '';
       }
     }
   }
+
+
+
+
+
+
+
 
 // function toggleTableRows(checkbox) {
 //     const rows = document.querySelectorAll('table tbody tr');
